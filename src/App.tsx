cdbf22699bdd3c456fd98b98ba865515eba8f5d1
Noorie-ctrl <alimotabiona@gmail.com>
2026-02-03 10:35:32 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+<h1 className="text-3xl font-bold">
+Tailwind v4 works 😀
+</h1>
+    </div>
+  )
+}
+
+export default App
